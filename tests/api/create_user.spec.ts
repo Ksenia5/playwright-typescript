b860bb314960bd3f138e0f9c1d87dbc9ddd164be
Ksenia5 <ogz.568458555@gmail.com>
2025-03-27 10:44:27 +0200

    comment fix

--- tests/api/create_user.spec.ts
+++ tests/api/create_user.spec.ts
@@ -1,27 +1,26 @@
 import { test, expect } from '@playwright/test'
 import { User } from '../../support/Models/User'
 
-test('Create user for signup', async ({ request }) => {
+test.only('Create user for signup', async ({ request }) => {
     const newUser = User.newUser();
-    const createUser = await request.post('https://gitlab.testautomate.me/api/v4/users', {
+    const response = await request.post('https://gitlab.testautomate.me/api/v4/users', {
         headers: {
             'Accept': 'application/json',
             'Authorization': 'Bearer FKzy_BpV5wAybKf7Z9JX',
         },
         data: {
-            "name": newUser.firstname,
-            "lastname": newUser.lastname,
-            "username": newUser.username,
-            "email": newUser.useremail,
-            "password": newUser.userpassword,
-            "skip_confirmation": true
+            name: newUser.firstname,
+            lastname: newUser.lastname,
+            username: newUser.username,
+            email: newUser.useremail,
+            password: newUser.userpassword,
+            skip_confirmation: true
         }
     });
-    expect(createUser.status()).toBe(201);
-    const responseBody = await createUser.json();
-    expect(responseBody.username).toBe(newUser.username);
+    expect(response.status()).toBe(201);
+    expect((await response.json()).username).toBe(newUser.username);
 
-    const getUser = await request.get(`https://gitlab.testautomate.me/api/v4/users/${responseBody.id}`, {
+    const getUser = await request.get(`https://gitlab.testautomate.me/api/v4/users/${(await response.json()).id}`, {
         headers: {
             'Authorization': 'Bearer FKzy_BpV5wAybKf7Z9JX',
         }
